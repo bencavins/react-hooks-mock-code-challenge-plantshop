3bed5ccfdf87b3a8ed542745351d2d7e3e4e011e
Ben Cavins <ben.cavins@flatironschool.com>
2023-02-08 14:42:22 -0500

    add button logic

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({name, image, price}) {
+  const [isInStock, setIsInStock] = useState(true)
+
+  function handleClick(event) {
+    setIsInStock(prev => !prev)
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button 
+          className="primary"
+          onClick={handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );

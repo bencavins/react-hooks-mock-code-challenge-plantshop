13bf6824798c4be41bbfdb724e469acc63aec930
Ben Cavins <ben.cavins@flatironschool.com>
2023-09-20 15:07:47 -0400

    do lab

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ id, image, name, price }) {
+  const [isInStock, setIsInStock] = useState(true)
+  const [priceData, setPriceData] = useState(price)
+
+  function handleClick() {
+    setIsInStock(prev => !prev)
+  }
+
+  function handleChange(event) {
+    setPriceData(event.target.value)
+
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        price: event.target.value
+      })
+    })
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      {/* <p>Price: {price}</p> */}
+      <input value={priceData} type="number" onChange={handleChange} />
+      {isInStock ? (
+        <button 
+          className="primary"
+          onClick={handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
